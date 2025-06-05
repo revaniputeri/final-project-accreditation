7a494d41127a493cc25a20863dea66cea65cedb1
zidnafaz <zidnafaza27@gmail.com>
2025-06-05 10:42:37 +0700

    change daftar isi

--- resources/views/dokumen_akhir/daftar_isi.blade.php
+++ resources/views/dokumen_akhir/daftar_isi.blade.php
@@ -1,11 +1,23 @@
 <div style="font-size: 20px; margin: 0px;">
     <h2>Daftar Isi</h2>
     <ul>
-        @foreach($daftarIsiList as $item)
+        @php
+            $grouped = collect($daftarIsiList)->groupBy('no_kriteria');
+        @endphp
+        @foreach($grouped as $no_kriteria => $items)
             <li>
-                <a href="{{ $item['link'] }}" style="text-decoration: none; color: rgb(0, 0, 0); cursor: pointer;">
-                    {{ $item['judul'] }} ... {{ $item['kategori'] }}
+                <a href="#kriteria-{{ $no_kriteria }}" style="text-decoration: none; color:rgb(0, 0, 0)e; cursor: pointer;">
+                    {{ $items->first()['judul'] }}
                 </a>
+                <ul style="margin-left: 5px;">
+                    @foreach($items as $item)
+                        <li>
+                            <a href="{{ $item['link'] }}" style="text-decoration: none; color: rgb(0, 0, 0); cursor: pointer;">
+                                {{ $item['kategori'] }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
             </li>
         @endforeach
     </ul>
