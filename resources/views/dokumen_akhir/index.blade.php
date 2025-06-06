7eca8675d3b7fc99ed4ca9d0c2a14096be17194b
zidnafaz <zidnafaza27@gmail.com>
2025-06-06 10:13:05 +0700

    change from /home to /dashboard

--- resources/views/dokumen_akhir/index.blade.php
+++ resources/views/dokumen_akhir/index.blade.php
@@ -6,7 +6,7 @@
     <div class="container-fluid">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{ url('/') }}">Beranda</a></li>
+                <li class="breadcrumb-item"><a href="{{ url('/dashboard') }}">Beranda</a></li>
                 <li class="breadcrumb-item active">Dokumen Akhir</li>
             </ol>
         </nav>
@@ -14,13 +14,13 @@
 @endsection
 
 @section('content')
-<div class="container-fluid p-0">
-    @if(isset($error))
-        <div class="alert alert-danger text-center mt-4">{{ $error }}</div>
-    @elseif(isset($pdfUrl))
-        <iframe src="{{ $pdfUrl }}" style="width: 100%; height: 90vh; border: none;"></iframe>
-    @else
-        <div class="alert alert-warning text-center mt-4">Tidak ada dokumen untuk ditampilkan.</div>
-    @endif
-</div>
+    <div class="container-fluid p-0">
+        @if (isset($error))
+            <div class="alert alert-danger text-center mt-4">{{ $error }}</div>
+        @elseif(isset($pdfUrl))
+            <iframe src="{{ $pdfUrl }}" style="width: 100%; height: 90vh; border: none;"></iframe>
+        @else
+            <div class="alert alert-warning text-center mt-4">Tidak ada dokumen untuk ditampilkan.</div>
+        @endif
+    </div>
 @endsection
