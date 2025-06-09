426966877ede84ccedcbf5689d351c84af1c7ff5
Revani Nanda Putri <revaninandaputeri@gmail.com>
2025-06-09 17:57:31 +0700

    feat: create new data kriteria

--- resources/views/kriteria/index.blade.php
+++ resources/views/kriteria/index.blade.php
@@ -15,179 +15,186 @@
 @endsection
 
 @section('content')
-<div class="container-fluid">
-    <div class="card shadow-sm">
-        <div class="card-header bg-primary border-bottom">
-            <div class="d-flex justify-content-between align-items-center">
-                <h3 class="card-title mb-0 text-white">Daftar Kriteria</h3>
-                <div class="card-tools">
-                    <a id="exportPdfBtn" class="btn btn-custom-blue me-2" href="{{ route('kriteria.export_pdf') }}">
-                        <i class="fa-solid fa-file-pdf me-2"></i> Export PDF
-                    </a>
-                    <a id="exportExcelBtn" class="btn btn-custom-blue me-2" href="{{ route('kriteria.export_excel') }}">
-                        <i class="fas fa-file-excel me-2"></i> Export Excel
-                    </a>
-                    <button class="btn btn-custom-blue me-2" onclick="modalAction('{{ route('kriteria.import') }}')">
-                        <i class="fa-solid fa-file-arrow-up me-2"></i> Import Data
-                    </button>
-                    <button onclick="modalAction('{{ route('kriteria.create_ajax') }}')" class="btn btn-custom-blue">
-                        <i class="fas fa-plus me-2"></i> Tambah Data
-                    </button>
+    <div class="container-fluid">
+        <div class="card shadow-sm">
+            <div class="card-header bg-primary border-bottom">
+                <div class="d-flex justify-content-between align-items-center">
+                    <h3 class="card-title mb-0 text-white">Daftar Kriteria</h3>
+                    <div class="card-tools">
+                        <a id="exportPdfBtn" class="btn btn-custom-blue me-2" href="{{ route('kriteria.export_pdf') }}">
+                            <i class="fa-solid fa-file-pdf me-2"></i> Export PDF
+                        </a>
+                        <a id="exportExcelBtn" class="btn btn-custom-blue me-2" href="{{ route('kriteria.export_excel') }}">
+                            <i class="fas fa-file-excel me-2"></i> Export Excel
+                        </a>
+                        <button class="btn btn-custom-blue me-2" onclick="modalAction('{{ route('kriteria.import') }}')">
+                            <i class="fa-solid fa-file-arrow-up me-2"></i> Import Data
+                        </button>
+                        <button onclick="modalAction('{{ route('kriteria.create_ajax') }}')" class="btn btn-custom-blue">
+                            <i class="fas fa-plus me-2"></i> Tambah Data
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body">
+                <div class="table-responsive">
+                    {{ $dataTable->table([
+                        'id' => 'kriteria-table',
+                        'class' => 'table table-hover table-bordered table-striped',
+                        'style' => 'width:100%',
+                    ]) }}
                 </div>
             </div>
         </div>
-
-        <div class="card-body">
-            <div class="table-responsive">
-                {{ $dataTable->table([
-                    'id' => 'kriteria-table',
-                    'class' => 'table table-hover table-bordered table-striped',
-                    'style' => 'width:100%',
-                ]) }}
+        <div id="myModal" class="modal fade" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <!-- Konten modal akan diisi secara dinamis -->
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 @push('scripts')
-{!! $dataTable->scripts() !!}
-<script>
-    function modalAction(url) {
-        $.get(url)
-            .done(function(response) {
-                $('#myModal .modal-content').html(response);
-                $('#myModal').modal('show');
+    {!! $dataTable->scripts() !!}
+    <script>
+        function modalAction(url) {
+            $.get(url)
+                .done(function (response) {
+                    $('#myModal .modal-content').html(response);
+                    $('#myModal').modal('show');
 
-                $(document).off('submit', '#formCreateKriteria, #formEditKriteria, #form-import');
+                    $(document).off('submit', '#formCreateKriteria, #formEditKriteria, #form-import');
 
-                $(document).on('submit', '#formCreateKriteria, #formEditKriteria', function(e) {
-                    e.preventDefault();
-                    var form = $(this);
+                    $(document).on('submit', '#formCreateKriteria, #formEditKriteria', function (e) {
+                        e.preventDefault();
+                        var form = $(this);
 
-                    $.ajax({
-                        url: form.attr('action'),
-                        method: form.find('input[name="_method"]').val() || form.attr('method'),
-                        data: form.serialize(),
-                        headers: {
-                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                        },
-                        success: function(res) {
-                            $('#myModal').modal('hide');
-                            window.LaravelDataTables["kriteria-table"].ajax.reload();
-                            if (res.alert && res.message) {
-                                Swal.fire({
-                                    icon: res.alert,
-                                    title: res.alert === 'success' ? 'Sukses' : 'Error',
-                                    text: res.message,
-                                    timer: 2000,
-                                    showConfirmButton: false
-                                });
+                        $.ajax({
+                            url: form.attr('action'),
+                            method: form.find('input[name="_method"]').val() || form.attr('method'),
+                            data: form.serialize(),
+                            headers: {
+                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                            },
+                            success: function (res) {
+                                $('#myModal').modal('hide');
+                                window.LaravelDataTables["kriteria-table"].ajax.reload();
+                                if (res.alert && res.message) {
+                                    Swal.fire({
+                                        icon: res.alert,
+                                        title: res.alert === 'success' ? 'Sukses' : 'Error',
+                                        text: res.message,
+                                        timer: 2000,
+                                        showConfirmButton: false
+                                    });
+                                }
+                            },
+                            error: function (xhr) {
+                                $('#myModal').modal('hide');
+                                window.LaravelDataTables["kriteria-table"].ajax.reload();
+                                if (xhr.responseJSON && xhr.responseJSON.alert && xhr.responseJSON.message) {
+                                    Swal.fire({
+                                        icon: xhr.responseJSON.alert,
+                                        title: xhr.responseJSON.alert === 'success' ? 'Sukses' : 'Error',
+                                        text: xhr.responseJSON.message,
+                                        timer: 2000,
+                                        showConfirmButton: false
+                                    });
+                                } else {
+                                    Swal.fire('Error!', 'Gagal menyimpan data karena duplikat Kode Kriteria.', 'error');
+                                }
                             }
-                        },
-                        error: function(xhr) {
-                            $('#myModal').modal('hide');
-                            window.LaravelDataTables["kriteria-table"].ajax.reload();
-                            if (xhr.responseJSON && xhr.responseJSON.alert && xhr.responseJSON.message) {
-                                Swal.fire({
-                                    icon: xhr.responseJSON.alert,
-                                    title: xhr.responseJSON.alert === 'success' ? 'Sukses' : 'Error',
-                                    text: xhr.responseJSON.message,
-                                    timer: 2000,
-                                    showConfirmButton: false
-                                });
-                            } else {
-                                Swal.fire('Error!', 'Gagal menyimpan data karena duplikat Kode Kriteria.', 'error');
-                            }
-                        }
+                        });
                     });
-                });
 
-                $(document).off('submit', '#form-import');
+                    $(document).off('submit', '#form-import');
 
-                $(document).on('submit', '#form-import', function(e) {
-                    e.preventDefault();
-                    var form = $(this);
-                    var formData = new FormData(form[0]);
-                    var submitBtn = form.find('button[type="submit"]');
+                    $(document).on('submit', '#form-import', function (e) {
+                        e.preventDefault();
+                        var form = $(this);
+                        var formData = new FormData(form[0]);
+                        var submitBtn = form.find('button[type="submit"]');
 
-                    submitBtn.prop('disabled', true).html('<i class="fas fa-spinner fa-spin me-1"></i> Memproses...');
+                        submitBtn.prop('disabled', true).html('<i class="fas fa-spinner fa-spin me-1"></i> Memproses...');
 
-                    $.ajax({
-                        url: form.attr('action'),
-                        method: 'POST',
-                        data: formData,
-                        processData: false,
-                        contentType: false,
-                        success: function(response) {
-                            $('#myModal').modal('hide');
-                            if (response.alert && response.message) {
-                                Swal.fire({
-                                    icon: response.alert,
-                                    title: response.alert === 'success' ? 'Sukses' : 'Error',
-                                    text: response.message,
-                                    timer: 2000,
-                                    showConfirmButton: false
-                                }).then(() => {
-                                    window.LaravelDataTables["kriteria-table"].ajax.reload();
-                                });
+                        $.ajax({
+                            url: form.attr('action'),
+                            method: 'POST',
+                            data: formData,
+                            processData: false,
+                            contentType: false,
+                            success: function (response) {
+                                $('#myModal').modal('hide');
+                                if (response.alert && response.message) {
+                                    Swal.fire({
+                                        icon: response.alert,
+                                        title: response.alert === 'success' ? 'Sukses' : 'Error',
+                                        text: response.message,
+                                        timer: 2000,
+                                        showConfirmButton: false
+                                    }).then(() => {
+                                        window.LaravelDataTables["kriteria-table"].ajax.reload();
+                                    });
+                                }
+                            },
+                            error: function (xhr) {
+                                $('#myModal').modal('hide');
+                                if (xhr.responseJSON && xhr.responseJSON.alert && xhr.responseJSON.message) {
+                                    Swal.fire({
+                                        icon: xhr.responseJSON.alert,
+                                        title: xhr.responseJSON.alert === 'success' ? 'Sukses' : 'Error',
+                                        text: xhr.responseJSON.message,
+                                        timer: 2000,
+                                        showConfirmButton: false
+                                    });
+                                } else {
+                                    Swal.fire({
+                                        icon: 'error',
+                                        title: 'Error',
+                                        text: xhr.responseJSON.message
+                                    });
+                                }
+                            },
+                            complete: function () {
+                                submitBtn.prop('disabled', false).html('<i class="fas fa-upload me-1"></i> Upload');
                             }
-                        },
-                        error: function(xhr) {
-                            $('#myModal').modal('hide');
-                            if (xhr.responseJSON && xhr.responseJSON.alert && xhr.responseJSON.message) {
-                                Swal.fire({
-                                    icon: xhr.responseJSON.alert,
-                                    title: xhr.responseJSON.alert === 'success' ? 'Sukses' : 'Error',
-                                    text: xhr.responseJSON.message,
-                                    timer: 2000,
-                                    showConfirmButton: false
-                                });
-                            } else {
-                                Swal.fire({
-                                    icon: 'error',
-                                    title: 'Error',
-                                    text: xhr.responseJSON.message
-                                });
-                            }
-                        },
-                        complete: function() {
-                            submitBtn.prop('disabled', false).html('<i class="fas fa-upload me-1"></i> Upload');
-                        }
+                        });
                     });
+                })
+                .fail(function (xhr) {
+                    Swal.fire('Error!', 'Gagal memuat form: ' + xhr.statusText, 'error');
                 });
-            })
-            .fail(function(xhr) {
-                Swal.fire('Error!', 'Gagal memuat form: ' + xhr.statusText, 'error');
+        }
+
+        $(document).on('submit', '#formDeleteKriteria', function (e) {
+            e.preventDefault();
+            var form = $(this);
+            $.ajax({
+                url: form.attr('action'),
+                method: 'POST',
+                data: form.serialize(),
+                success: function (response) {
+                    $('#myModal').modal('hide');
+                    window.LaravelDataTables["kriteria-table"].ajax.reload();
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Berhasil',
+                        text: 'Kriteria berhasil dihapus.',
+                        timer: 2000,
+                        showConfirmButton: false
+                    });
+                },
+                error: function (xhr) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Gagal',
+                        text: 'Tidak dapat menghapus kriteria karena masih digunakan.'
+                    });
+                }
             });
-    }
-
-    $(document).on('submit', '#formDeleteKriteria', function(e) {
-        e.preventDefault();
-        var form = $(this);
-        $.ajax({
-            url: form.attr('action'),
-            method: 'POST',
-            data: form.serialize(),
-            success: function(response) {
-                $('#myModal').modal('hide');
-                window.LaravelDataTables["kriteria-table"].ajax.reload();
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Berhasil',
-                    text: 'Kriteria berhasil dihapus.',
-                    timer: 2000,
-                    showConfirmButton: false
-                });
-            },
-            error: function(xhr) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Gagal',
-                    text: 'Tidak dapat menghapus kriteria karena masih digunakan.'
-                });
-            }
         });
-    });
-</script>
+    </script>
 @endpush
